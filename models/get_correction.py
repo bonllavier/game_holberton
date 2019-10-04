70bf680b0dc4971e5794536e61124fb773d4f1dd
miguelfajardoc <775@holbertonschool.com>
2019-10-04 17:16:46 +0000

    remove trash, add consolidate checkers info

--- models/get_correction.py
+++ models/get_correction.py
@@ -34,7 +34,7 @@ def get_correction_id(id_task, auth_token):
     correction_json = url_response.json()
 
     if ('error' in correction_json):
-        print('error correction')
+        print(correction_json.get('error'))
         correction_json = None
         return None
     return (correction_json.get('id'))
